0ee433562aadad346f27b9baaaffee7942206043
Eduardo Calderon <ejcalderon@gmail.com>
2020-09-20 18:35:41 -0500

    Branch fixes (#2)
    * ignore cookies
    
    * update 1.1.0
    
    add json for custom input. change to write download links to output file instead of a powershell script
    
    Co-authored-by: EJ <eduardo.calderon@dematic.com>

--- DramaScrape.py
+++ DramaScrape.py
@@ -3,6 +3,8 @@ import requests
 import re
 import urllib.request
 import sys
+import os
+import json
 
 def parseCookieFile(cookiefile):
     """Parse a cookies.txt file and return a dictionary of key value pairs
@@ -16,21 +18,52 @@ def parseCookieFile(cookiefile):
                 cookies[lineFields[5]] = lineFields[6]
     return cookies
 
-cookies = parseCookieFile('DramaCoolCookies.txt')
-url = ""
 
-for i in range(1,42):
-    url = 'https://www3.dramacool.movie/1-night-2-days-s04-episode-'+str(i)+'.html'
-    r = requests.get(url, cookies=cookies)
-    soup = BeautifulSoup(r.text,"html.parser")
+#Start Code
+try:
+    cookies = parseCookieFile('DramaCoolCookies.txt')
+    url = ""
+    f = open('URLList.txt','w')
 
-    #Need to Emulate the Javascript Call to find all the Download Links: document.getElementsByClassName('cf-download')[0].children[2].getAttribute("href").toString()
-    try:
-        mydivs = soup.find("div", {"class": "cf-download"}).findAll("a", href=True, text=True, recursive=False)
+    #Load JSON for Variables
+    with open('config.json','r') as read_file:
+        data = json.load(read_file)
 
-        #Format URL for Powershell Script for youtube-dl. It looks like below
-        #Start-Process .\youtube-dl.exe '"URL" --sub-lang "en,eng" --write-sub -o "\OutputFolder\%(title)s.%(ext)s"'
-        print("Start-Process .\youtube-dl.exe '\"%s\"" % mydivs[len(mydivs) - 1]['href'] + " --sub-lang \"en,eng\" --write-sub -o \"\\2D1N\%(title)s.%(ext)s\"'")
+    URLStart = data["URLStart"]
+    URLEnd = data["URLEnd"]
+    
+    start = data["LoopStartEnd"][0]
+    end = data["LoopStartEnd"][1]
 
-    except:
-        print("Unexcepted Error:", sys.exc_info()[0])
+    for i in range(start,end):
+        #Debug Print just to keep track during Compilation
+        print(i)
+
+        urlArray = []
+        
+        for k in range(1,len(URLStart)+1):
+            urlArray.append(URLStart[k-1]+str(i)+URLEnd[k-1])
+
+        #In Cases where we have multiple base URLs for a show, let's loop through it and exit if we get a valid link
+        for url in urlArray:
+            r = requests.get(url, cookies=cookies)
+
+            if(r.status_code == 200):
+                break
+
+        soup = BeautifulSoup(r.text,"html.parser")
+
+        #Need to Emulate the Javascript Call to find all the Download Links: document.getElementsByClassName('cf-download')[0].children[2].getAttribute("href").toString()
+        try:
+            mydivs = soup.find("div", {"class": "cf-download"}).findAll("a", href=True, text=True, recursive=False)
+            f.write(mydivs[len(mydivs) - 1]['href']+'\n')
+
+        except:
+            print("Unexcepted Error:", sys.exc_info()[0])
+
+except:
+    print("Unexcepted Error:", sys.exc_info()[0])
+
+f.close()
+
+    
--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 Basic Functionality to Extract VODs on DramaCool
 
-Cookies.text is accessed from Logging into Site and using the Cookies.TXT Chrome Extension
+Cookies.text is accessed from Logging into Site and generating the Cookies.TXT Chrome Extension
 
-The Output is a List of Execution Statements to be run in Powershell using the youtube-dl Application
+config.json is used to pass the URLs (as an array if there are multiple URLs to support) and Loop Start/End
+
+Executing the Python Scripts writes to a file URLList.txt
+
+We can utilize XARGS to pipe all the URLs to be batch downloaded
+
+i.e xargs -n 1 -P ${jobs} wget < /PathToFile/URLList.txt
 
