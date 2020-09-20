80235a488af808c108fcf07e1f82eb872fe79741
EJ Calderon <ejcalderon@gmail.com>
2020-09-20 14:54:55 -0500

    add a bit of error handling

--- DramaScrape.py
+++ DramaScrape.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import urllib.request
+import sys
 
 def parseCookieFile(cookiefile):
     """Parse a cookies.txt file and return a dictionary of key value pairs
@@ -24,11 +25,12 @@ for i in range(1,42):
     soup = BeautifulSoup(r.text,"html.parser")
 
     #Need to Emulate the Javascript Call to find all the Download Links: document.getElementsByClassName('cf-download')[0].children[2].getAttribute("href").toString()
-    mydivs = soup.find("div", {"class": "cf-download"}).findAll("a", href=True, text=True, recursive=False)
+    try:
+        mydivs = soup.find("div", {"class": "cf-download"}).findAll("a", href=True, text=True, recursive=False)
 
-    #Format URL for Powershell Script for youtube-dl. It looks like below
-    #Start-Process .\youtube-dl.exe '"http://www.mtv.com/episodes/j8g97l/the-challenge-total-madness-mad-world-season-35-ep-3501" --sub-lang "en,eng" --write-sub -o "\The Challenge S35\%(title)s.%(ext)s"'
-    print("Start-Process .\youtube-dl.exe '\"%s\"" % mydivs[len(mydivs) - 1]['href'] + " --sub-lang \"en,eng\" --write-sub -o \"\\2D1N\%(title)s.%(ext)s\"'")
-    
-    #print("Start-Process .\youtube-dl.exe '""" + mydivs[len(mydivs) - 1]['href'] + """ --sub-lang ""en,eng"" --write-sub -o ""\2D1N\%(title)s.%(ext)s""'""")
-    #print("Start-Process .\youtube-dl.exe '\"%s\"" % url + " --sub-lang \"en,eng\" --write-sub -o \"\\2D1N\%(title)s.%(ext)s\"'")
+        #Format URL for Powershell Script for youtube-dl. It looks like below
+        #Start-Process .\youtube-dl.exe '"URL" --sub-lang "en,eng" --write-sub -o "\OutputFolder\%(title)s.%(ext)s"'
+        print("Start-Process .\youtube-dl.exe '\"%s\"" % mydivs[len(mydivs) - 1]['href'] + " --sub-lang \"en,eng\" --write-sub -o \"\\2D1N\%(title)s.%(ext)s\"'")
+
+    except:
+        print("Unexcepted Error:", sys.exc_info()[0])
