c2f1960726989dc3e3f58e92b7ecb5dfb127dd46
EJ Calderon <ejcalderon@gmail.com>
2020-09-20 14:46:56 -0500

    add cookies and download links
    add cookies, add href link downloads, print in powershell function already

--- DramaScrape.py
+++ DramaScrape.py
@@ -1,18 +1,34 @@
-import webbrowser
+from bs4 import BeautifulSoup
+import requests
+import re
+import urllib.request
 
-wb = webbrowser.open("https://www3.dramacool.movie/1-night-2-days-s04-episode-1.html", new=2)
-wb.document.getElementsByClassName('cf-download')[0].children[2].getAttribute("href").toString()
+def parseCookieFile(cookiefile):
+    """Parse a cookies.txt file and return a dictionary of key value pairs
+    compatible with requests."""
 
-#Login to Site To Get Timer to Pick Up After Restart
-#$ie = New-Object -ComObject 'internetExplorer.Application'
-#$ie.Visible= $true # Make it visible
-#
-#$ie.Navigate("https://www3.dramacool.movie/1-night-2-days-s04-episode-1.html")
-#
-#While ($ie.Busy -eq $true) {Start-Sleep -Seconds 3;}
-#
-#$Link = $ie.document.getElementsByClassName('cf-download')[0].children[2].getAttribute("href").toString()
-#$Link.click()
-#
-#$ie.Quit()
+    cookies = {}
+    with open (cookiefile, 'r') as fp:
+        for line in fp:
+            if not re.match(r'^\#', line):
+                lineFields = line.strip().split('\t')
+                cookies[lineFields[5]] = lineFields[6]
+    return cookies
 
+cookies = parseCookieFile('DramaCoolCookies.txt')
+url = ""
+
+for i in range(1,42):
+    url = 'https://www3.dramacool.movie/1-night-2-days-s04-episode-'+str(i)+'.html'
+    r = requests.get(url, cookies=cookies)
+    soup = BeautifulSoup(r.text,"html.parser")
+
+    #Need to Emulate the Javascript Call to find all the Download Links: document.getElementsByClassName('cf-download')[0].children[2].getAttribute("href").toString()
+    mydivs = soup.find("div", {"class": "cf-download"}).findAll("a", href=True, text=True, recursive=False)
+
+    #Format URL for Powershell Script for youtube-dl. It looks like below
+    #Start-Process .\youtube-dl.exe '"http://www.mtv.com/episodes/j8g97l/the-challenge-total-madness-mad-world-season-35-ep-3501" --sub-lang "en,eng" --write-sub -o "\The Challenge S35\%(title)s.%(ext)s"'
+    print("Start-Process .\youtube-dl.exe '\"%s\"" % mydivs[len(mydivs) - 1]['href'] + " --sub-lang \"en,eng\" --write-sub -o \"\\2D1N\%(title)s.%(ext)s\"'")
+    
+    #print("Start-Process .\youtube-dl.exe '""" + mydivs[len(mydivs) - 1]['href'] + """ --sub-lang ""en,eng"" --write-sub -o ""\2D1N\%(title)s.%(ext)s""'""")
+    #print("Start-Process .\youtube-dl.exe '\"%s\"" % url + " --sub-lang \"en,eng\" --write-sub -o \"\\2D1N\%(title)s.%(ext)s\"'")
